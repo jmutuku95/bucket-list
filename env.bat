adaf25f318b877bee9d8057e470a76037e66c987
Joseph Kyalo <jmutuku95@gmail.com>
2018-04-08 11:01:00 +0300

    environment settings

---
+++ env.bat
@@ -0,0 +1,5 @@
+../venv/Scripts/activate
+set FLASK_APP=run.py
+set SECRET=some-very-long-string-of-random-characters-CHANGE-TO-YOUR-LIKING
+set APP_SETTINGS=development
+set DATABASE_URL=postgresql://postgres:kyalo2018@127.0.0.1:5432/flask_api
